ecceed255d7a6829c177a3677775ded75ed002ea
sekeiryuutei <vburbano@live.com>
2020-07-24 21:02:55 -0500

    actualizo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAixUHezwh-G44CdwJqJI85B1ic7QRVlzs",
+    authDomain: "tienda-70cff.firebaseapp.com",
+    databaseURL: "https://tienda-70cff.firebaseio.com",
+    projectId: "tienda-70cff",
+    storageBucket: "tienda-70cff.appspot.com",
+    messagingSenderId: "606652847310",
+    appId: "1:606652847310:web:83dce52c70d1c78d552c9f",
+    measurementId: "G-JGSY3ZMP14"
+  },
 };
 
 /*
